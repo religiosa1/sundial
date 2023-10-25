9e637b30e2fab465864d57859a9de458e7f0390a
Viacheslav Azarov <public@religiosa.ru>
2023-10-25 20:28:43 +0200

    FIX: comment back date selector form

--- src/lib/components/Menu/menu.svelte
+++ src/lib/components/Menu/menu.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import { SubMenuEnum } from '$lib/models/SubMenuEnum';
+  import { SubMenuEnum } from "$lib/models/SubMenuEnum";
   import { AppRouteEnum } from "$lib/models/routes";
 
-  import DateForm from "./DateForm.svelte"
+  import DateForm from "./DateForm.svelte";
 
   let submenu = SubMenuEnum.none;
 
@@ -13,7 +13,7 @@
       } else {
         submenu = SubMenuEnum.none;
       }
-    }
+    };
   }
 </script>
 
@@ -55,28 +55,29 @@
       on:click={togglePlaceForm}
     />
     -->
-    <button
+    <!-- <button
       type="button"
       class="menu__button dateform"
       class:active={ submenu === SubMenuEnum.dateform }
       title="date"
       on:click={toggleSubmenu(SubMenuEnum.dateform)}
-    />
+    /> -->
   </div>
 </aside>
 
 {#if submenu === SubMenuEnum.dateform}
-  <DateForm on:close={() => submenu = SubMenuEnum.none} />
+  <DateForm on:close={() => (submenu = SubMenuEnum.none)} />
 {/if}
 
 <style>
-.menu {
-  position: absolute;
-  top: 10px;
-  right: 30px;
-  background: transparent;
-}
-  .menu__nav, .controls {
+  .menu {
+    position: absolute;
+    top: 10px;
+    right: 30px;
+    background: transparent;
+  }
+  .menu__nav,
+  .controls {
     display: flex;
     flex-flow: column nowrap;
     gap: 10px;
@@ -84,7 +85,7 @@
   }
   .menu__button {
     display: block;
-    width:50px;
+    width: 50px;
     height: 50px;
     margin: 0;
     padding: 0;
@@ -110,42 +111,53 @@
     content: "";
   }
 
-  .menu__button:active { transform: translate(1px, 1px); }
-  .menu__button.active { background-color: #999; }
+  .menu__button:active {
+    transform: translate(1px, 1px);
+  }
+  .menu__button.active {
+    background-color: #999;
+  }
 
-  .display-type_table::after { background-image: url("/img/table.svg"); }
-  .display-type_dial::after { background-image: url("/img/dial.svg"); }
+  .display-type_table::after {
+    background-image: url("/img/table.svg");
+  }
+  .display-type_dial::after {
+    background-image: url("/img/dial.svg");
+  }
   /* FIXME отдельная картинка для этого раздела  */
-  .display-type_yearly::after { background-image: url("/img/calendar.svg"); }
-
+  .display-type_yearly::after {
+    background-image: url("/img/calendar.svg");
+  }
 
   /* .placeform { background-image: url("/img/globe.svg"); } */
-  .dateform::after { background-image: url("/img/calendar.svg"); }
-@media (prefers-color-scheme: dark) {
-  .menu__button {
-    background-color: #0E0E0E;
-    box-shadow: 0 0 20px #2E2E2E inset;
+  .dateform::after {
+    background-image: url("/img/calendar.svg");
   }
-  .menu__button::after {
-    filter: invert(1) brightness(0.9);
+  @media (prefers-color-scheme: dark) {
+    .menu__button {
+      background-color: #0e0e0e;
+      box-shadow: 0 0 20px #2e2e2e inset;
+    }
+    .menu__button::after {
+      filter: invert(1) brightness(0.9);
+    }
   }
-}
-@media (max-width: 700px) {
-  .menu {
-    top: auto;
-    bottom: 10px;
-    right: 10px;
-    left: 10px;
+  @media (max-width: 700px) {
+    .menu {
+      top: auto;
+      bottom: 10px;
+      right: 10px;
+      left: 10px;
+    }
+    .menu__nav {
+      flex-direction: row;
+      gap: 25px;
+      justify-content: space-around;
+      justify-content: space-evenly;
+    }
+    .menu__button {
+      width: 40px;
+      height: 40px;
+    }
   }
-  .menu__nav {
-    flex-direction: row;
-    gap: 25px;
-    justify-content: space-around;
-    justify-content: space-evenly;
-  }
-  .menu__button {
-    width: 40px;
-    height: 40px;
-  }
-}
 </style>
